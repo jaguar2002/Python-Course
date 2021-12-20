60c4c548da3e2a5a973056ffcfd13398eaf1b55f
Sameer Pabreja <ares97666@gmail.com>
2021-12-20 18:32:21 +0530

    loops part added

---
+++ Conditionals/IfStatements.py
@@ -0,0 +1,7 @@
+def hint_username(username):
+	if len(username < 3):
+		print("Invalid username. Must be at least 3 characters long")
+	else:
+		return print("Valid Username")
+
+hint_username("Hello")
