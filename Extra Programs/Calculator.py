fa2d3d82aa98dd5c1a90e7dbb33fde38039a8743
Sameer Pabreja <ares97666@gmail.com>
2021-12-20 19:13:14 +0530

    some programs of my own

---
+++ Extra Programs/Calculator.py
@@ -0,0 +1,6 @@
+print("enter the first number")
+a = input()
+print ("enter the second number")
+b = input()
+c = a + b
+print("the answer is " + str(c))
--- Loops/WhileLoops.py
+++ Loops/WhileLoops.py
@@ -2,4 +2,16 @@ my_variable = 5
 while my_variable < 10:
 	print("Hello")
 	my_variable += 1
-	
+
+x = 1
+sum = 0
+while x < 10:
+	sum += x
+	x += 1
+
+product = 1
+while x < 10:
+	product = product * x
+	x += 1
+
+print("Sum is " + str(sum) + " Product is " + str(product)) 
